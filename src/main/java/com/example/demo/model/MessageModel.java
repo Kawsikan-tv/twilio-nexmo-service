d26bd81a792a667ce2cd72e295e17e92a589d6c0
Kawsikan-tv <kawsikan.kumaralingam@techventuras.com>
2022-04-11 21:38:48 +0530

    added lombok

--- src/main/java/com/example/demo/model/MessageModel.java
+++ src/main/java/com/example/demo/model/MessageModel.java
@@ -1,33 +1,11 @@
 package com.example.demo.model;
 
-public class MessageModel {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class MessageModel {
     private String mobileNumber;
     private String smsText;
-
-    public MessageModel() {
-        super();
-    }
-
-    public MessageModel(String mobileNumber, String smsText) {
-        super();
-        this.mobileNumber = mobileNumber;
-        this.smsText = smsText;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getSmsText() {
-        return smsText;
-    }
-
-    public void setSmsText(String smsText) {
-        this.smsText = smsText;
-    }
 }
